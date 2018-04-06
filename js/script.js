eec108f6ca4947eea9ea4509d95d7c664a126fc7
tcortin <tcortin@gmail.com>
2018-04-06 09:59:07 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* Animation de la scrollbar au changement de section */
+$(function(){
+
+    $(".nav a, footer a").on("click", function(event){
+
+        event.preventDefault();
+        var hash = this.hash;
+
+        $('body, html').animate({scrollTop: $(hash).offset().top}, 900 , function(){window.location.hash = hash;})
+
+    });
+
+});
